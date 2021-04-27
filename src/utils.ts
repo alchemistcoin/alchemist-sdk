16c0769c93cb4c63c31605026250b8ab573bf206
Dire <diredeaths@gmail.com>
2021-04-27 10:30:31 -0700

    comment warning

--- src/utils.ts
+++ src/utils.ts
@@ -14,7 +14,8 @@ export function validateSolidityTypeInstance(value: JSBI, solidityType: Solidity
 export function validateAndParseAddress(address: string): string {
   try {
     const checksummedAddress = getAddress(address)
-    warning(address === checksummedAddress, `${address} is not checksummed.`)
+    // this needs to be uncommented before prod MVP
+    // warning(address === checksummedAddress, `${address} is not checksummed.`)
     return checksummedAddress
   } catch (error) {
     invariant(false, `${address} is not a valid address.`)
