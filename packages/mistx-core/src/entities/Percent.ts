5e4b429f130b901824e2227c374c35e8949e8894
juice <ju1cecfr@gmail.com>
2021-12-02 22:36:02 -0800

    add mistx-univ3 package

--- packages/mistx-core/src/entities/Percent.ts
+++ packages/mistx-core/src/entities/Percent.ts
@@ -1,15 +1,44 @@
-import { Rounding, _100 } from '../constants'
+import { Rounding, _100, BigintIsh} from '../constants'
 import { Fraction } from './Fraction'
 
 const _100_PERCENT = new Fraction(_100)
 
+/**
+ * Converts a fraction to a percent
+ * @param fraction the fraction to convert
+ */
+ function toPercent(fraction: Fraction): Percent {
+  return new Percent(fraction.numerator, fraction.denominator)
+}
+
 export class Percent extends Fraction {
+  /**
+   * This boolean prevents a fraction from being interpreted as a Percent
+   */
+   public readonly isPercent: true = true
+
+   add(other: Fraction | BigintIsh): Percent {
+     return toPercent(super.add(other))
+   }
+ 
+   subtract(other: Fraction | BigintIsh): Percent {
+     return toPercent(super.subtract(other))
+   }
+ 
+   multiply(other: Fraction | BigintIsh): Percent {
+     return toPercent(super.multiply(other))
+   }
+ 
+   divide(other: Fraction | BigintIsh): Percent {
+     return toPercent(super.divide(other))
+   }
+
   public toSignificant(
     significantDigits: number = 5,
     format?: object,
     rounding?: Rounding
   ): string {
-    return this.multiply(_100_PERCENT).toSignificant(
+    return super.multiply(_100_PERCENT).toSignificant(
       significantDigits,
       format,
       rounding
@@ -21,6 +50,6 @@ export class Percent extends Fraction {
     format?: object,
     rounding?: Rounding
   ): string {
-    return this.multiply(_100_PERCENT).toFixed(decimalPlaces, format, rounding)
+    return super.multiply(_100_PERCENT).toFixed(decimalPlaces, format, rounding)
   }
 }
--- packages/mistx-core/src/entities/natives/Ether.ts
+++ packages/mistx-core/src/entities/natives/Ether.ts
@@ -1,4 +1,5 @@
-import { Token, WETH } from '../Token'
+import { Token } from '../Token'
+import { WETH } from 'entities/weth'
 
 import { Currency } from '../Currency'
 import { NativeCurrency } from '../NativeCurrency'
